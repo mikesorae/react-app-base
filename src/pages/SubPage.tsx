28de9c22ac45a4bd012940b7130c8fa62fd08191
mikesorae <lovepoko247@gmail.com>
2019-09-08 22:00:03 +0900

    add sample code using hooks

--- src/pages/SubPage.tsx
+++ src/pages/SubPage.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { mainLayout } from '../layouts/MainLayout';
 
-const Content = () => (
-  <p>Sub Page</p>
-);
+const Content = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <>
+      <p>Sub Page</p>
+      <p>{ count }</p>
+      <div>
+        <button type="button" onClick={increment}>+</button>
+        <button type="button" onClick={decrement}>-</button>
+      </div>
+    </>
+  );
+};
 
 export const SubPage = mainLayout(Content);
